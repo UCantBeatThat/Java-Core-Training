12b772484a0f0fe37c95a9dd1c1e0707caf371fa
UCantBeatThat <hebbarsandesh98@gmail.com>
2020-11-09 20:00:43 +0530

    JUnit Basic Examples

---
+++ Examples/JUnit Example/test/com/training/tests/TestGreeting.java
@@ -0,0 +1,40 @@
+package com.training.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.training.Greeting;
+
+@DisplayName(value="First JUnit Test Case")
+class TestGreeting {
+
+	private Greeting greeting = new Greeting();
+	
+	@Test
+	@Disabled(value="Not used")
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	@DisplayName(value="Test Message is not null")
+	void testGetMessageNotNull() {
+		
+		String actual = greeting.getMessage();
+		
+		assertNotNull(actual);
+	}
+	
+	@Test
+	@DisplayName(value="Test for getMessage to return Hello World")
+	void testGetMessageReturnsString() {
+		
+		String actual = greeting.getMessage();
+		
+		assertEquals("Welcome to Java Training", actual);
+	}
+
+}
