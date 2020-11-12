46079fce992db5bcc5ff846ec72516177df2214d
UCantBeatThat <hebbarsandesh98@gmail.com>
2020-11-12 09:48:00 +0530

    Adding Core Java Project Submission Files

---
+++ CaseStudySubmission/dailysalesreport/src/main/java/com/project/organicstores/dailysalesreport/services/ReportGeneratorService.java
@@ -0,0 +1,86 @@
+package com.project.organicstores.dailysalesreport.services;
+
+import java.sql.Connection;
+import java.time.LocalDate;
+import java.util.List;
+
+import com.project.organicstores.dailysalesreport.model.Product;
+
+public class ReportGeneratorService {
+	
+	private Connection connection;
+	
+	public ReportGeneratorService(Connection connection) {
+		super();
+		this.connection = connection;
+	}
+
+	public void generateReportByDate(int key, LocalDate date) {
+		
+		ProductService productService = new ProductService(connection);
+		
+		List<Product> productList = productService.findByDate(key, date);
+		
+		double grandTotal = 0.0;
+		int totalQuantity = 0;
+		
+		if(key == 1) {
+			System.out.println("Edible Items report for " + date.getDayOfMonth() + " " + date.getMonth() + " " + date.getYear());
+		}
+		else if(key == 2) {
+			System.out.println("Garment Items report for " + date.getDayOfMonth() + " " + date.getMonth() + " " + date.getYear());
+		}
+		else if(key == 3) {
+			System.out.println("Electronic Items report for " + date.getDayOfMonth() + " " + date.getMonth() + " " + date.getYear());
+		}
+		System.out.println("--------------------------------------------------------------------------");
+		System.out.println("Sl. No.\tItem Name\t\tUnit Price\tQuantity\tAmount\t");
+		System.out.println("--------------------------------------------------------------------------");
+		
+		for(Product eachProduct : productList) {
+			grandTotal += eachProduct.getLineTotal();
+			totalQuantity += eachProduct.getQuantity();
+			System.out.println(eachProduct.getItemCode() + "\t" + eachProduct.getItemName() + "\t\t" 
+					+ eachProduct.getUnitPrice() + "\t\t" + eachProduct.getQuantity() + "\t\t" + eachProduct.getLineTotal());
+		}
+		
+		System.out.println("--------------------------------------------------------------------------");
+		System.out.println("Total\t\t\t\t\t\t" + totalQuantity + "\t\t" + grandTotal);
+		System.out.println("--------------------------------------------------------------------------\n\n");
+	}
+	
+	public void generateReportForTopSellingItems(int key, int month) {
+		
+		ProductService productService = new ProductService(connection);
+		
+		List<Product> productList = productService.findTopProductsByMonth(key, month);
+		
+		final String[] monthNames = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+		
+		int totalQuantity = 0;
+		
+		if(key == 1) {
+			System.out.println("Top Selling Edible Items for " + monthNames[month-1] + " 2020");
+		}
+		else if(key == 2) {
+			System.out.println("Top Selling Garment Items for " + monthNames[month-1] + " 2020");
+		}
+		else if(key == 3) {
+			System.out.println("Top Selling Electronic Items for " + monthNames[month-1] + " 2020");
+		}
+		System.out.println("---------------------------------------------------");
+		System.out.println("Sl. No.\tItem Name\t\tQuantity");
+		System.out.println("---------------------------------------------------");
+		
+		for(Product eachProduct : productList) {
+			totalQuantity += eachProduct.getQuantity();
+			System.out.println(eachProduct.getItemCode() + "\t" + eachProduct.getItemName() + "\t\t" 
+					+ eachProduct.getQuantity());
+		}
+		
+		System.out.println("---------------------------------------------------");
+		System.out.println("Total\t\t\t\t" + totalQuantity);
+		System.out.println("---------------------------------------------------\n\n");
+		
+	}
+}
---
+++ CaseStudySubmission/dailysalesreport/src/test/java/com/project/organicstores/dailysalesreport/TestReportServices.java
@@ -0,0 +1,59 @@
+package com.project.organicstores.dailysalesreport;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestReportServices {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testFindLastKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindTopProductsByMonth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCloseConnection() {
+		fail("Not yet implemented");
+	}
+
+}
